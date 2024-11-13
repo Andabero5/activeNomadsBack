04c4a23c05ee448b4fde0cd2ae70963ebea5a9c2
Andrés Beltrán <andres5354317@gmail.com>
2024-11-13 11:37:31 -0500

    cors

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const { Timestamp } = require('firebase-admin/firestore');
 const events = require('./events.json');
 
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-
 app.use(cors({
     origin: 'https://active-nomads.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+app.use(bodyParser.json());
+
+
 
 async function loadEvents() {
     const eventsRef = admin.firestore().collection('events');
